c97c7a9c4f820711f3cb46c19ff489f420e64a6f
İbrahim Gündüz <ibrahim.gunduz@modanisa.com>
2022-08-02 16:08:58 +0300

    objective-c supported

--- Pod/Classes/BannerGroupViewModel.swift
+++ Pod/Classes/BannerGroupViewModel.swift
@@ -6,8 +6,8 @@
 
 import Foundation
 
-@objc public class BannerGroupViewModel{
-    @objc public init() {}
-    @objc public var group:String?
+public class BannerGroupViewModel{
+    public init() {}
+    public var group:String?
     public var banners:[InternalBannerModel]?
 }
