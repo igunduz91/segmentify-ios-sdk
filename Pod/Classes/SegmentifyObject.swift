b8e4e3114c1595f8b386e58d9f6403968722eb94
İbrahim Gündüz <ibrahim.gunduz@modanisa.com>
2023-02-27 10:27:46 +0300

    Update SegmentifyObject.swift

--- Pod/Classes/SegmentifyObject.swift
+++ Pod/Classes/SegmentifyObject.swift
@@ -4,14 +4,14 @@
 
 import Foundation
 
-public class SegmentifyObject:NSObject {
-    public override init() {}
-    public var sessionID:String?
-    public var pageUrl:String?
-    public var currency:String?
-    public var lang:String?
-    public var params:[String:AnyObject]?
+@objc public class SegmentifyObject:NSObject {
+    @objc public override init() {}
+    @objc public var sessionID:String?
+    @objc public var pageUrl:String?
+    @objc public var currency:String?
+    @objc public var lang:String?
+    @objc public var params:[String:AnyObject]?
     public var nextPage: Bool?
     public var testMode: Bool?
-    public var region: String?
+    @objc public var region: String?
 }
